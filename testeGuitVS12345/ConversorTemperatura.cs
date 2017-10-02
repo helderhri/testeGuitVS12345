2e5871a659cb08407a6c436e05e375d2b2a044c6
helderhri <ricardohri@hotmail.com>
2017-10-02 08:29:17 -0300

    Add project files.

---
+++ testeGuitVS12345/ConversorTemperatura.cs
@@ -0,0 +1,15 @@
+﻿namespace testeGuitVS12345
+{
+    public class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura)
+        {
+            return 0;
+        }
+
+        public static double FahrenheitParaKelvin(double temperatura)
+        {
+            return 0;
+        }
+    }
+}
